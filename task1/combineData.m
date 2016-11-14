22d84dfaab60e94e6c576f656a1a45905b8dae52
saumya1510 <saumya1510@gmail.com>
2016-11-13 19:59:25 -0700

    Update to include cell numbers in the matrix

--- task1/combineData.m
+++ task1/combineData.m
@@ -3,6 +3,8 @@ function [dataMatrix] = combineData(inputMap)
     cellKeys = keys(inputMap);
     for i = 1:length(cellKeys)
         eachFrame = inputMap(char(cellKeys(i)));
+        cellkeys = i * ones(size(eachFrame, 1), 1);
+        eachFrame = horzcat(cellkeys, eachFrame);
         dataMatrix = vertcat(dataMatrix, eachFrame);
     end
 end
