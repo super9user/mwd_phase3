a7bca487296f43881b4c83afdee205c03399a462
Tanmay Patil <tanmay.ashok.patil@gmail.com>
2016-11-25 01:27:37 -0700

    Cleaned up indentation of code

--- task3/findSummation.m
+++ task3/findSummation.m
@@ -1,21 +1,21 @@
-function summation=  findSummation(obj,GM,GIM) % current object, GM=globalPageRankMap, GIM= globalIncomingMatrix
+function summation=  findSummation(obj, GM, GIM) % current object, GM=globalPageRankMap, GIM= globalIncomingMatrix
 
-summation=0;
-targetV=obj.videoNum;
-targetF=obj.frameNum;
-videostr=num2str(targetV);
-framestr=num2str(targetF);
-mapKey=strcat(videostr,',',framestr);
-if(isKey(GIM, mapKey))
-    objectArray=GIM(mapKey);
-    [u, v]= size(objectArray);
-
-    for i=1:v
-        adjNode=objectArray(i);
+    summation=0;
+    targetV=obj.videoNum;
+    targetF=obj.frameNum;
+    videostr=num2str(targetV);
+    framestr=num2str(targetF);
+    mapKey=strcat(videostr,',',framestr);
+    
+    if(isKey(GIM, mapKey))
+        objectArray=GIM(mapKey);
+        [~, v]= size(objectArray);
+        for i=1:v
+            adjNode=objectArray(i);
             wx=adjNode.simValue;
             [pageRank, outweigh]=findPageRank(adjNode.sourceVideo,adjNode.sourceFrame,GM);
             summation=summation+(wx*pageRank/outweigh);
+        end
     end
     
-end
 end
--- task3/task3.m
+++ task3/task3.m
@@ -1,94 +1,94 @@
 function [ orderedPageRank ] = task3( videoDirectory, dataFileName, m)
-text=fileread(dataFilename);
-text = strrep(text,'<','');
-text = strrep(text,'>','');
-text = strrep(text,'{','');
-text = strrep(text,'}','');
 
-fileID = fopen('temp.txt','w');
-fprintf(fileID,text);
-fclose(fileID);
-M = csvread('temp.txt');
+    text=fileread(dataFileName);
+    text = strrep(text,'<','');
+    text = strrep(text,'>','');
+    text = strrep(text,'{','');
+    text = strrep(text,'}','');
 
-[numPoints y]=size(M);
-k=5;
-numPoint
-z=1;
-m=4;
-d=0.85;
-totalNodes = numPoints/k;
-clear videoRefMatrix;
+    fileID = fopen('temp.txt','w');
+    fprintf(fileID,text);
+    fclose(fileID);
+    M = csvread('temp.txt');
 
-globalIncomingMatrix=containers.Map(); % map where incoming node is the key
-globalPageRankMap= containers.Map(); % map where every node( 'video,frame' is a key)
+    [numPoints, ~]=size(M);
+    k=5;
+    numPoint
+    z=1;
 
-for i=1:totalNodes
-    
-    for j=1:k    
-        x=M(z+j-1,1:5);
-        videostr=num2str(x(3));
-        framestr=num2str(x(4));
-        keystr=strcat(videostr,',',framestr);
-        
-        o=VideoReference(x(1),x(2),x(3),x(4),x(5));
-        videoRefMatrix(i,j)=o;
-        if(~isKey(globalIncomingMatrix, keystr))
-        	globalIncomingMatrix(keystr) = [o];
-        
-        else
-            array=globalIncomingMatrix(keystr);
-            array=[array o];
-            globalIncomingMatrix(keystr)=array;
+    d=0.85;
+    totalNodes = numPoints/k;
+    clear videoRefMatrix;
+
+    globalIncomingMatrix=containers.Map(); % map where incoming node is the key
+    globalPageRankMap= containers.Map(); % map where every node( 'video,frame' is a key)
+
+    for i=1:totalNodes
+        for j=1:k    
+            x=M(z+j-1,1:5);
+            videostr=num2str(x(3));
+            framestr=num2str(x(4));
+            keystr=strcat(videostr,',',framestr);
+
+            o=VideoReference(x(1),x(2),x(3),x(4),x(5));
+            videoRefMatrix(i,j)=o;
+            if(~isKey(globalIncomingMatrix, keystr))
+                globalIncomingMatrix(keystr) = [o];
+            else
+                array=globalIncomingMatrix(keystr);
+                array=[array o];
+                globalIncomingMatrix(keystr)=array;
+            end
         end
-        
+        z=z+k;
     end
-    z=z+k;
-end
 
-for i=1:totalNodes
-    obj=videoRefMatrix(i);
-    sum=0;
-    for j=1:k
-        sum=sum+videoRefMatrix(i,j).simValue;
+    for i=1:totalNodes
+        obj=videoRefMatrix(i);
+        sum=0;
+        for j=1:k
+            sum=sum+videoRefMatrix(i,j).simValue;
+        end
+        videoNum=obj.sourceVideo;
+        strVideo=num2str(videoNum);
+        frameNum=obj.sourceFrame;
+        strNum=num2str(frameNum);
+        pagerankvalue=1/numPoints/k;
+        mapKey=strcat(strVideo,',',strNum);
+        globalPageRankMap(mapKey)= VideoNode(videoNum,frameNum,pagerankvalue,sum);  
+        pageRankMatrix(i)=VideoNode(videoNum,frameNum,pagerankvalue,sum);
     end
-    videoNum=obj.sourceVideo;
-    strVideo=num2str(videoNum);
-    frameNum=obj.sourceFrame;
-    strNum=num2str(frameNum);
-    pagerankvalue=1/numPoints/k;
-    mapKey=strcat(strVideo,',',strNum);
-    globalPageRankMap(mapKey)= VideoNode(videoNum,frameNum,pagerankvalue,sum);  
-    pageRankMatrix(i)=VideoNode(videoNum,frameNum,pagerankvalue,sum);
-end
 
-for i=1:100 % no of iterations
-    i
-   for j=1:totalNodes       
-    obj=pageRankMatrix(j);
-    strVideo=num2str(obj.videoNum);
-    strFrame=num2str(obj.frameNum);
-    mapK=strcat(strVideo,',',strFrame);
-    summation=findSummation(obj,globalPageRankMap,globalIncomingMatrix);
-    pagerank=1-d+d*summation;
-    globalPageRankMap(mapK)= VideoNode(obj.videoNum,obj.frameNum,pagerank,obj.outweight);  
-   end
-end
-% [~,idx]=sort([pageRankMatrix.pageRankValue],'descend');
-% orderedPageRank=pageRankMatrix(idx);
-% 
-% for j=1:m
-%     pageRankObject=orderedPageRank(j);
-%     videoNum=pageRankObject.videoNum;
-%     frameNum=pageRankObject.frameNum;
-%     vidKey=globalVideoIndex(num2str(videoNum));
-%     videoObj=VideoReader(strcat('P2DemoVideos/',vidKey));
-%    img=read(videoObj,frameNum);
-%    pageRank=strcat(' PageRank - ',num2str(pageRankObject.pageRankValue)); 
-%    videostr=strcat (' Video Number - ',num2str(videoNum));
-%    videoName=strcat(' Video Name - ',vidKey);
-%    frameName=strcat (' Frame Number - ',num2str(frameNum));
-%    title(strcat(videoName,videostr,frameName,pageRank));
-%    figure, imshow(img);
-%    imwrite(img,'pageRankFrames.tif','WriteMode','append');
-% end
+    for i=1:100 % no of iterations
+        i
+        for j=1:totalNodes       
+            obj=pageRankMatrix(j);
+            strVideo=num2str(obj.videoNum);
+            strFrame=num2str(obj.frameNum);
+            mapK=strcat(strVideo,',',strFrame);
+            summation=findSummation(obj,globalPageRankMap,globalIncomingMatrix);
+            pagerank=1-d+d*summation;
+            globalPageRankMap(mapK)= VideoNode(obj.videoNum,obj.frameNum,pagerank,obj.outweight);
+        end
+    end
+    
+%     [~,idx]=sort([pageRankMatrix.pageRankValue],'descend');
+%     orderedPageRank=pageRankMatrix(idx);
+
+%     for j=1:m
+%         pageRankObject=orderedPageRank(j);
+%         videoNum=pageRankObject.videoNum;
+%         frameNum=pageRankObject.frameNum;
+%         vidKey=globalVideoIndex(num2str(videoNum));
+%         videoObj=VideoReader(strcat(videoDirectory,vidKey));
+%         img=read(videoObj,frameNum);
+%         pageRank=strcat(' PageRank - ',num2str(pageRankObject.pageRankValue)); 
+%         videostr=strcat (' Video Number - ',num2str(videoNum));
+%         videoName=strcat(' Video Name - ',vidKey);
+%         frameName=strcat (' Frame Number - ',num2str(frameNum));
+%         title(strcat(videoName,videostr,frameName,pageRank));
+%         figure, imshow(img);
+%         imwrite(img,'pageRankFrames.tif','WriteMode','append');
+%     end
+    
 end
