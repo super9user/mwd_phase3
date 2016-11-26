c4791ed8a0ce10ff6c67dd9782979c0672682ded
Tanmay Patil <tanmay.ashok.patil@gmail.com>
2016-11-25 21:24:20 -0700

    Cleaner and basic version of PPR

--- task4/personalizedPageRank.m
+++ task4/personalizedPageRank.m
@@ -1,29 +1,21 @@
 function [ pv ] = personalizedPageRank(  )
 
     X = [[0 1 0 0 0]; [1 0 0 0 0 ]; [0 1 0 0 0]; [0 0 1 0 1]; [0 0 0 1 0]];
-    [n, p] = size(X);
+    [n, ~] = size(X);
     Seed = 1; % Node 1 is a seed node
-    alpha = 0.85;
+    beta = 0.15;
     maxIter = 100;
     
     E = zeros(1,n);
     for i=1:n
         if(i == Seed)
-            E(1, i) = 1;
+            E(1, i) = 1; % This shouldn't be one. Needs to change.
         end
     end
-    V = ones(1,n) * 1/n; % Default. Needs to change;
-
-    rowsumvector = ones(1,n) * X';
-    nonzerorows = find(rowsumvector);
-    zerorows = setdiff(1:n, nonzerorows);
-    l = length(zerorows);
-    a = sparse(zerorows, ones(l,1), ones(l,1), n, 1); % dangling node vector
-    pv=E;
+    pv = ones(1,n) * 1/n; % Default (This is biased & sensitive to outliers)
 
     for i=1:maxIter
-        pv = alpha*pv*X + (alpha*(pv*a)+1-alpha)*V;
+        pv = (1 - beta)*pv*X + beta*E;
     end
     
 end
-
